ba5c2ca3302bc26894457b5d67cdf9a4658708ba
steve-th247 <steve_thow1573@hotmail.com>
2022-05-30 14:32:49 +0800

    Modified vpts_rules3.c
    Wrong indices were used

--- ex00/vpts_rules3.c
+++ ex00/vpts_rules3.c
@@ -24,13 +24,13 @@ void	apply_vpts_rule3(short int possi_grid[][4], short int *vpts)
 			possi_grid[0][i] = possi_grid[0][i]
 				& get_mask_n_bit(4 - (vpts[i] - 1));
 		else if (i >= 4 && i <= 7)
-			possi_grid[3][i - 3] = possi_grid[3][i - 3]
+			possi_grid[3][i % 4] = possi_grid[3][i % 4]
 				& get_mask_n_bit(4 - (vpts[i] - 1));
 		else if (i >= 8 && i <= 11)
-			possi_grid[i - 7][0] = possi_grid[i - 7][0]
+			possi_grid[i % 4][0] = possi_grid[i % 4][0]
 				& get_mask_n_bit(4 - (vpts[i] - 1));
 		else if (i >= 12 && i <= 15)
-			possi_grid[i - 11][3] = possi_grid[i - 11][3]
+			possi_grid[i % 4][3] = possi_grid[i % 4][3]
 				& get_mask_n_bit(4 - (vpts[i] - 1));
 		i++;
 	}
